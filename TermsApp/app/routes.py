16354518ecee4a554d5f1d4944d3b118cb34be9e
claudiuzalinca <claudiu-serban.zalinca@student.tuiasi.ro>
2020-11-07 20:55:25 +0200

    Update

--- TermsApp/app/routes.py
+++ TermsApp/app/routes.py
@@ -13,10 +13,19 @@ def about():
 @app.route('/check/terms', methods=['POST'])
 def check():
     m = request.json
-    print(m["url"])
-    print(m["text"])
-    return m["url"]+m["text"]
-
+    dict={}
+    paragraphs = m["text"].split('\n\n')
+    text="<div>"
+    sumResult=0
+    for p in paragraphs:
+      print(p)
+      #apelat functia pentru paragraf
+      dict[p]=0
+      sumResult+=0
+      text=text+"<div><p>"+p+"</p><label>Result:</label><p>"+str(0)+"</p></div>"
+    print(dict)
+    text+="</div>"
+    return text
 
 if __name__ == '__main__':
   app.run(debug=True)
